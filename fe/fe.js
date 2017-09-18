18512a66e2d656bae2620a8e6ece93ae25e674fd
poshtarboba <poshtar.boba@gmail.com>
2017-09-18 18:17:22 +0300

    fe new lections

--- fe/fe.js
+++ fe/fe.js
@@ -18,6 +18,22 @@ function toggleBox(handles){
 	handles.forEach(function(handle){
 		handle.addEventListener('click', function(){
 			let box = document.querySelector('#' + this.dataset.id);
+			if (box.classList.contains('animating')) { return; }
+			if (box.offsetHeight) {
+				box.dataset.height = box.offsetHeight;
+			}
+			else {
+				box.style.height = '';
+				box.dataset.height = box.offsetHeight;
+				box.style.height = '0';
+			}
+			box.dataset.iteration = 0;
+			box.dataset.animtype = 'show';
+			box.dataset.timerid = setInterval(function(box){
+				let progress = box.dataset.iteration / 20;
+				 if (box.dataset.animtype === 'close') { progress = 1 - progress; }
+				 box.style.height = progress * box.dataset.height;
+			}, 20, box);
 		});
 	});
 }
