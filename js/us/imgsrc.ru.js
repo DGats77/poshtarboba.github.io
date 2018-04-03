2e226f79b97c1a9767d53f71bbd2069728813078
boba <poshtarboba@toavalon.com>
2018-04-04 02:10:25 +0300

    userscripts

--- js/us/imgsrc.ru.js
+++ js/us/imgsrc.ru.js
@@ -5,47 +5,50 @@ TODO:
 
 */
 
-(function(){
+(function() {
 	
-	/* ховер-эффект для таблиц */
-	if (location.pathname === '/main/user.php' || location.pathname.substr(0, 5) === '/cat/') addTableCSS();
+	removeSomeBlocks(); // удалить разные ненужные элементы
+	btnNextClick(); // автопереход по кнопке "Продолжить просмотр"
+	linkToAllImages(); // добавить ссылку на все фото альбома
+	imgLoadBigImage(document); // загрузить большие версии изображений
+	loadAllImages(); // загрузить все изображения на одной странице
 	
-	/* авторелоад списка категории */
-	if (location.pathname.substr(0, 5) === '/cat/') catAutoReload();
+	if (isCatPage()) {
+		addTableCSS(); // ховер-эффект для таблиц
+		catAutoReload(); // авторелоад списка категории
+	}
+	if (isUserPage()) {
+		addTableCSS(); // ховер-эффект для таблиц
+	}
 	
-	/* автопереход по кнопке "Продолжить просмотр" */
-	let btnNext = document.querySelector('[value="Продолжить просмотр"]');
-	if (btnNext) { console.log('Button next'); btnNext.parentElement.submit(); }
 	
-	/* загрузить большие версии изображений */
-	imgLoadBigImage(document);
+	function isCatPage(){ return location.pathname.substr(0, 5) === '/cat/'; }
+	function isUserPage(){ return location.pathname === '/main/user.php'; }
 	
-	/* добавить ссылку на все фото альбома */
-	linkToAllImages();
-	
-	/* загрузить все изображения на одной странице */
-	loadAllImages();
-	
-	function addTableCSS(){
+	function addTableCSS() {
 		console.log('Add hover for table');
-		let html = '.zebra tr:hover > td { background-color: #c5ccd0; }';
-		let style = document.createElement('style');
-		style.innerText = html;
+		let style = createTag('style');
+		style.innerText = '.zebra tr:hover > td { background-color: #c5ccd0; }';
 		document.head.appendChild(style);
-		document.querySelectorAll('table').forEach(function(table){
-			table.classList.add('zebra');
+		$$('table th').forEach(function(th) {
+			if (th.innerText === 'фото') th.parentElement.parentElement.parentElement.classList.add('zebra');
 		});
 	}
 	
-	function catAutoReload(){
+	function btnNextClick() {
+		let btnNext = $1('[value="Продолжить просмотр"]');
+		if (btnNext) btnNext.parentElement.submit();
+	}
+	
+	function catAutoReload() {
 		console.log('autoreload');
-		let a = document.querySelector('a[href="/main/search.php"]');
+		let a = $1('a[href="/main/search.php"]');
 		let c = Math.floor(Math.random() * 241 + 60);
-		let span = document.createElement('span');
+		let span = createTag('span');
 		span.style.paddingRight = '12px';
 		span.innerText = c;
 		a.parentElement.insertBefore(span, a);
-		let i = setInterval(function(){
+		let i = setInterval(function() {
 			span.innerText = --c;
 			if (c <= 0) {
 				clearInterval(i);
@@ -54,9 +57,9 @@ TODO:
 		}, 1000);
 	}
 	
-	function imgLoadBigImage(parent){
+	function imgLoadBigImage(parent) {
 		let i = 0;
-		parent.querySelectorAll('img[data-src]').forEach(function(img){
+		$$('img[data-src]', parent).forEach(function(img) {
 			img.setAttribute('src', img.dataset.src);
 			img.classList.remove('lazyload');
 			delete img.dataset.src;
@@ -65,50 +68,99 @@ TODO:
 		if (i) console.log('Loaded big images:', i);
 	}
 	
-	function linkToAllImages(){
-		document.querySelectorAll('a[href]').forEach(function(a){
+	function linkToAllImages() {
+		$$('a[href]').forEach(function(a) {
 			let href = a.getAttribute('href');
 			if (href.substr(0, 14) === '/main/tape.php') {
 				console.log('Add link to all images');
 				a.innerText = 'все фото постранично';
-				let span = document.createElement('span');
+				let span = createTag('span');
 				span.innerHTML = ' | <a href="' + href + '&showall=true">все фото альбома</a>';
 				a.parentElement.insertBefore(span, a.nextElementSibling);
 			}
 		});
 	}
 	
-	function loadAllImages(){
+	function loadAllImages() {
 		let search = location.search.substr(1).split('&');
 		if (search.includes('showall=true')) {
 			let href = [];
-			document.querySelectorAll('a.navdot[href]').forEach(function(a){
+			$$('a.navdot[href]').forEach(function(a) {
 				let url = a.getAttribute('href');
-				href.forEach(function(item){ if (item === url) url = null; });
+				href.forEach(function(item) { if (item === url) url = null; });
 				if (url) href.push(url);
 			});
 			if (href.length) console.log('Load all images, found pages:', href.length + 1);
+			let style = createTag('style');
+			style.innerHTML = 'img.big { display: inline-block; margin-bottom: 12px; } .images-page { padding: 20px 0;}';
+			document.head.appendChild(style);
+			let div = createTag('div');
+			div.classList.add('images-page');
+			let html = '';
+			$$('img.big').forEach(function(img) { html += img.outerHTML + '<br>'; });
+			div.innerHTML = html + ;
+			document.body.appendChild(div);
 			getImagesPage(href);
 		}
 	}
 	
-	function getImagesPage(href){
+	function getImagesPage(href) {
 		if (href.length === 0) return false;
 		console.log('Get next page, total left:', href.length);
 		GetURI(href.shift(), function(xhr){
 			if (xhr.status === 200){
 				let resp = xhr.response.match(/<body>([\s\S]*)<\/body>/gim);
 				if (!resp[0]) return;
-				let div = document.createElement('div');
+				let div = createTag('div');
+				div.classList.add('images-page');
 				div.innerHTML = resp[0].replace(/<\/?body>/g, '');
 				imgLoadBigImage(div);
 				let html = '';
-				div.querySelectorAll('img.big').forEach(function(img){ html += img.outerHTML + '<br>'; })
+				$$('img.big', div).forEach(function(img){ html += img.outerHTML + '<br>'; })
 				div.innerHTML = html;
 				document.body.appendChild(div);
 			}
 			getImagesPage(href);
 		});
+	}
+	
+	function removeSomeBlocks() {
+		removeScripts(); // удалить скрипты
+		removeFromHeader(); // удалить элементы из хедера
+		removeFromFooter(); // удалить элементы из футера
+		if (isCatPage()) removeFineText(); // удалить красивый текст на страницах категорий
+		
+		function removeScripts() {
+			$$('body script').forEach(function(script) { script.remove(); });
+			$$('noscript').forEach(function(noscript) { noscript.remove(); });
+			$$('link[href*="flags.css"]').forEach(function(link) { link.remove(); });
+		}
+		function removeFromHeader() {
+			$$('img.badge').forEach(function(img) { img.parentElement.innerHTML = "[ Logout ]" });
+			$$('.topmenu a.tomato').forEach(function(a) { a.remove(); });
+			$$('a[href="/main/dudes.php"]').forEach(function(a) { a.remove(); });
+			$$('tr.topmenu td[align="right"]').forEach(function(td) { td.innerHTML = td.innerHTML.replace(/\|/g, '&nbsp;'); });
+			$$('td[width="100"] img[width="100"][height="100"]').forEach(function(img) { img.parentElement.remove(); });
+		}
+		function removeFromFooter() {
+			$1('td.bottomline').parentElement.remove();
+			$$('body > a').forEach(function(a) { a.remove(); });
+		}
+		function removeFineText() {
+			let formSearch = $1('form[name="srch"]');
+			if (!formSearch) return;
+			let h2, p;
+			while (true) {
+				h2 = formSearch.nextElementSibling;
+				p = h2.nextElementSibling;
+				if (h2 && h2.tagName.toLowerCase() !== 'h2') break;
+				if (p && p.tagName.toLowerCase() !== 'p') break;
+				if (h2.innerText === 'Подразделы') break;
+				if (h2.innerText === 'Популярные теги раздела') break;
+				h2.remove();
+				p.remove();
+			}
+		}
 	}
 	
 })();
--- js/us/main.js
+++ js/us/main.js
@@ -1,11 +1,18 @@
-window.GetURI = _GetURI;
+let GetURI = _fx_GetURI;
+let add0 = _fx_add0;
+let $$ = _fx_$$;
+let $1 = _fx_$1;
+let createTag = _fx_createTag;
+let addClass = _fx_addClass;
+let removeClass = _fx_removeClass;
+let toggleClass = _fx_toggleClass;
 
 (function(){
 	
 	if (location.host === 'imgsrc.ru') addScript('imgsrc.ru.js');
 	
 	function addScript(fileName){
-		let script = document.createElement('script');
+		let script = createTag('script');
 		document.documentElement.appendChild(script);
 		script.setAttribute('src', 'https://poshtarboba.github.io/js/us/' + fileName);
 		console.log('Added script', fileName);
@@ -13,7 +20,7 @@ window.GetURI = _GetURI;
 	
 })();
 
-function _GetURI(uri, handle){
+function _fx_GetURI(uri, handle){
 	let xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function(){
 		if (xhr.readyState !== 4) return;
@@ -22,3 +29,11 @@ function _GetURI(uri, handle){
 	xhr.open('GET', uri, true);
 	xhr.send();
 };
+
+function _fx_add0(n) { return n < 10 ? '0' + n : n.toString(); }
+function _fx_$$(selector, parent) { if (!parent) parent = document; return parent.querySelectorAll(selector); }
+function _fx_$1(selector, parent) { if (!parent) parent = document; return parent.querySelector(selector); }
+function _fx_createTag(tag) { return document.createElement(tag); }
+function _fx_addClass(elements, classes) { if (!elements) return; classes = classes.split(/\s+/); elements.forEach(function(elem) { className.forEach(function(className) { elem.classList.add(className); }) }); }
+function _fx_removeClass(elements, classes) { if (!elements) return; classes = classes.split(/\s+/); elements.forEach(function(elem) { className.forEach(function(className) { elem.classList.remove(className); }) }); }
+function _fx_toggleClass(elements, classes) { if (!elements) return; classes = classes.split(/\s+/); elements.forEach(function(elem) { className.forEach(function(className) { elem.classList.toggle(className); }) }); }
